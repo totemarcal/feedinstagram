0ab0502894f7457b569ecfe77067f927e62ffd74
totemarcal <totemarcal@gmail.com>
2020-11-03 15:37:40 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Feed from './src/pages/Feed';
+import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native'
+
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <View style={style.container}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Feed">
+          <Stack.Screen name="Feed" component={Feed} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+  );
+}
+
+const style = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      backgroundColor: '#fff' 
+    }
+  }
+)
+
+
---
+++ src/pages/Feed/index.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, FlatList,Text , View} from 'react-native';
+import axios from 'axios'
+import LazyImage from '../../components/LazyImage';
+
+import { Container, Post, Header, Avatar, Name, Description, Loading } from './styles';
+
+export default function Feed() {
+  const [error, setError] = useState('');
+  const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [viewable, setViewable] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  async function loadPage(pageNumber = page, shouldRefresh = false) {
+    if (pageNumber === total) return;
+    if (loading) return;
+
+    setLoading(true);
+    //http://localhost:3000/feed?_expand=author&_limit=4&_page=1
+    //utilizar server.js no jsonserver
+    //https://5fa103ace21bab0016dfd97e.mockapi.io/api/v1/feed?page=1&limit=4
+    //utilizar o server2.js no www.mockapi.io
+    axios
+    .get(`https://5fa103ace21bab0016dfd97e.mockapi.io/api/v1/feed?page=1&limit=4`)
+    .then(response => {
+      const totalItems = response.headers["x-total-count"]
+      const data = response.data
+      console.log(data)
+      setLoading(false)
+      setTotal(Math.floor(totalItems / 4));
+      setPage(pageNumber + 1);
+
+      setFeed(shouldRefresh ? data : [...feed, ...data]);
+
+    })
+    .catch(err => {
+      setError(err.message);
+      setLoading(true)
+    })
+  }
+
+  async function refreshList() {
+    setRefreshing(true);
+    
+    await loadPage(1, true);
+
+    setRefreshing(false);
+  }
+
+
+  useEffect(() => {
+    loadPage()
+  }, []);
+
+  const renderItem = ({item}) => {
+    return (
+      <Post>
+            <Header>
+              <Avatar source={{ uri: item.author.avatar }} />
+              <Name>{item.author.name}</Name>
+            </Header>
+
+            <LazyImage
+              aspectRatio={item.aspectRatio} 
+              shouldLoad={viewable.includes(item.id)} 
+              smallSource={{ uri: item.small }}
+              source={{ uri: item.image }}
+            />
+
+            <Description>
+              <Name>{item.author.name}</Name> {item.description}
+            </Description>
+          </Post>
+    )
+  }
+  
+  const handleViewableChanged = useCallback(({ changed }) => {
+    setViewable(changed.map(({ item }) => item.id));
+  }, []);
+
+  return (
+    <Container >
+      <FlatList
+        key="list"
+        data={feed}
+        keyExtractor={item => String(item.id)}
+        renderItem={renderItem}
+        ListFooterComponent={loading && <Loading />}
+        onViewableItemsChanged={handleViewableChanged}
+        viewabilityConfig={{
+          viewAreaCoveragePercentThreshold: 10,
+        }}
+        showsVerticalScrollIndicator={false}
+        onRefresh={refreshList}
+        refreshing={refreshing}
+        onEndReachedThreshold={0.1}
+        onEndReached={() => loadPage()}
+        ListFooterComponent={loading && <Loading />}
+
+      />
+    </Container>
+  );
+}
+
+
